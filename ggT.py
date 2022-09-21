40ba5637c5b3d9718858ba8dcd6015721872b378
Ronny Zavrtak <rzavrtak@ger-l7490nb010.lnx.commsolid.com>
2022-09-21 15:15:50 +0200

    add usage

--- ggT.py
+++ ggT.py
@@ -26,5 +26,18 @@ def rem(self, n, d):
         d=r
     return r    
 
+def usage():
+  print(f"The script {sys.argv[0]} needs to be called with 2 integer numbers a and b.")
+  print("example:")
+  print(f">{sys.argv[0]} 256 16")
+  print("ggT(256, 16) = 16")
+
+def mymethod(argv):
+ if(len(argv)<2):
+  usage()
+  sys.exit(2)
+if __name__=='__main__':
+        mymethod(sys.argv[1:])
+
 a = ggT(int(sys.argv[1]), int(sys.argv[2]))
 a.PrintggT()
